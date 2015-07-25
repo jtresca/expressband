aff34335620e38335b8430e2e1cb0d3a62bce11d
Joseph Tresca <jtresca@glgroup.com>
2015-07-25 02:18:38 -0400

    added telephone filtering, time and address handling

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -1,14 +1,21 @@
 'use strict';
 
 angular.module('expressbandApp')
-  .controller('AdminCtrl', function ($scope, $http, Auth, User, $location) {
+  .controller('AdminCtrl', function ($scope, $http, Auth, User, $location, $route, telFilter) {
 
+$scope.currencyVal;
+
+
+    $http.get('/api/showdates').success(function(dates) {
+      $scope.showdates = dates;
+    });
     // if(!Auth.isAdmin()) {
     //     $location.path('/')
     //     console.log("Blocked!");
     // }
 
     // Use the User $resource to fetch all users
+    $scope.adminlocation = $location.path();
     $scope.users = User.query();
 
     $scope.emailUser = function() {
@@ -36,11 +43,100 @@ angular.module('expressbandApp')
           town: $scope.newShow.town,
           zip: $scope.newShow.zip,
           date: $scope.newShow.date, 
-          time: $scope.newShow.time,
+          startTime: $scope.newShow.startTime,
+          endTime: $scope.newShow.endTime,
           phone: $scope.newShow.phone, 
           notes: $scope.newShow.notes });
        $scope.newShow = '';
+       console.log("TELL FILTER RAN ON SUBMIT:",telFilter($scope.newShow.phone));
+       $route.reload();
     };
 
 
   });
+
+//BEGIN TELEPHONE FILTERING
+angular.module('expressbandApp').directive('phoneInput', function($filter, $browser) {
+  return {
+    require: 'ngModel',
+    link: function($scope, $element, $attrs, ngModelCtrl) {
+      var listener = function() {
+        var value = $element.val().replace(/[^0-9]/g, '');
+        $element.val($filter('tel')(value, false));
+      };
+
+      // This runs when we update the text field
+      ngModelCtrl.$parsers.push(function(viewValue) {
+
+        return viewValue.replace(/[^0-9]/g, '').slice(0, 10);
+      });
+
+      // This runs when the model gets updated on the scope directly and keeps our view in sync
+      ngModelCtrl.$render = function() {
+        $element.val($filter('tel')(ngModelCtrl.$viewValue, false));
+      };
+
+      $element.bind('change', listener);
+      $element.bind('keydown', function(event) {
+        var key = event.keyCode;
+        // If the keys include the CTRL, SHIFT, ALT, or META keys, or the arrow keys, do nothing.
+        // This lets us support copy and paste too
+        if (key == 91 || (15 < key && key < 19) || (37 <= key && key <= 40)) {
+          return;
+        }
+        $browser.defer(listener); // Have to do this or changes don't get picked up properly
+      });
+
+      $element.bind('paste cut', function() {
+        $browser.defer(listener);
+            
+      });
+    }
+
+  };
+});
+angular.module('expressbandApp').filter('tel', function() {
+  return function(tel) {
+
+
+    if (!tel) {
+      return '';
+    }
+
+    var value = tel.toString().trim().replace(/^\+/, '');
+
+    if (value.match(/[^0-9]/)) {
+      return tel; 
+    }
+
+    var country, city, number;
+
+    switch (value.length) {
+      case 1:
+      case 2:
+      case 3:
+        city = value;
+        break;
+
+      default:
+        city = value.slice(0, 3);
+        number = value.slice(3);
+    }
+
+    if (number) {
+      if (number.length > 3) {
+        number = number.slice(0, 3) + '-' + number.slice(3, 7);
+
+      } else {
+        number = number;
+      }
+
+      return ("(" + city + ") " + number).trim();
+    } else {
+      return "(" + city;
+    }
+
+  };
+});
+
+//END TELEPHONE FILTERING
